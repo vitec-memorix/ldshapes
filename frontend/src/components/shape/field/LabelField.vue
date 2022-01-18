f3336655246e05762a5603509a6786816c14f902
Gera Knol <g.knol@picturae.com>
2022-01-18 12:31:13 +0100

    improved labelfield html

--- frontend/src/components/shape/field/LabelField.vue
+++ frontend/src/components/shape/field/LabelField.vue
@@ -1,18 +1,9 @@
 <template>
-    <div class="form-property col-12" v-if="inline === false">
-        <label class="form-label fw-bold">rdfs:label</label>
-        <div class="input-group">
-            <div class="labelinput-container form-control">
-                                <span v-for="(label, index) in list" :key="index" class="labelinput rounded-3">
-                        {{label.title}}:<span class="language">{{label.language}}</span> <span class="bi-x-lg" v-on:click="removeSettingRow(field,index)"></span>
-                    </span>
-            </div>
-            <button class="btn btn-primary pe-4 ps-4" type="button" data-bs-toggle="modal" data-bs-target="#rdfsLabelModal" @click="updateSettingField('modalField',field)">Add</button>
-        </div>
-    </div>
-    <div class="row mb-3" v-if="inline === true">
-        <label class="col-sm-2 col-form-label">rdfs:label</label>
-        <div class="col-sm-10">
+    <div :class="inline === true ? 'row mb-3' : 'form-property col-12'">
+        <label :class="inline === true ? 'col-sm-2 col-form-label' : 'form-label fw-bold'" >
+            rdfs:label
+        </label>
+        <div :class="inline === true ? 'col-sm-10' : ''">
             <div class="input-group">
                 <div class="labelinput-container form-control">
                     <span v-for="(label, index) in list" :key="index" class="labelinput rounded-3">
