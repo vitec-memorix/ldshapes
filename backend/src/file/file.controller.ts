f8e3f07c6f765c94eb330c4eb40daca0760bdc32
Gera Knol <g.knol@picturae.com>
2022-01-28 17:52:56 +0100

    added download file button

--- backend/src/file/file.controller.ts
+++ backend/src/file/file.controller.ts
@@ -1,10 +1,9 @@
-import { Controller, Post, UseInterceptors, UploadedFile, Req, Request } from '@nestjs/common';
+import { Controller, Post, UseInterceptors, UploadedFile, Req, Request, Get, Res, StreamableFile, Response, Query } from '@nestjs/common';
 import { FileService } from './file.service';
-import { diskStorage } from 'multer';
 import { FileInterceptor } from "@nestjs/platform-express";
-import * as fs from 'fs';
 import { multerOptions } from './file.inceptor';
-
+import { createReadStream } from 'fs';
+import { join } from 'path';
 
 @Controller('file')
 export class FileController {
@@ -15,12 +14,23 @@ export class FileController {
   }
 
   @Post()
-  // @Post(':folder')
   @UseInterceptors(FileInterceptor('file', multerOptions))
   async upload(@UploadedFile() file) {
     // console.log(req.body['omg'])
     console.log('done')
   }
+
+  @Get()
+  getFile(@Query() query): StreamableFile {
+    let filePath = '../resources';
+    if(query['folder']) {
+      filePath += query['folder'];
+    }
+    console.log(`${filePath}/${query['file']}`);
+    const file = createReadStream(`${filePath}/${query['file']}`);
+    return new StreamableFile(file);
+  }
+
   //
   // @Get()
   // async findAll() {
--- frontend/src/components/shape/PickSource.vue
+++ frontend/src/components/shape/PickSource.vue
@@ -80,6 +80,20 @@
         axios.delete(`${server.baseURL}/shape/${value}`).then(response => {
           this.fetchShapes();
         });
+      },
+      downloadShape(file:string) {
+        let params =  {
+          file: file,
+          folder:'/shapes'
+        };
+        axios.get(`${server.baseURL}/file`,{ params: params }).then(response => {
+          const url = window.URL.createObjectURL(new Blob([response.data]));
+          const link = document.createElement('a');
+          link.href = url;
+          link.setAttribute('download', file); //or any other extension
+          document.body.appendChild(link);
+          link.click();
+        });
       }
     }
   });</script>
