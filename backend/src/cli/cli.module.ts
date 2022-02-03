9547e4f5b6faa3234ea2660170917f05f01c07ed
Daan Mooij <daanmooij@gmail.com>
2022-02-03 12:59:16 +0100

    create combineFields function

--- backend/src/cli/cli.module.ts
+++ backend/src/cli/cli.module.ts
@@ -2,5 +2,7 @@ import { Module } from '@nestjs/common';
 import { TransformCommand } from './commands/transform.command';
 import { ValidateCommand } from './commands/validate.command';
 
-@Module({ providers: [TransformCommand, ValidateCommand] })
+@Module({
+  providers: [TransformCommand, ValidateCommand],
+})
 export class CliModule {}
--- backend/src/cli/commands/transform.command.ts
+++ backend/src/cli/commands/transform.command.ts
@@ -9,25 +9,17 @@ import * as path from 'path';
 export class TransformCommand implements CommandRunner {
   async run(
     inputs: string[],
-    options: { dataSource: string; mapping: string; output: string },
+    options: { mapping: string; output: string },
   ): Promise<void> {
     this.transform(options);
-  }
-
-  @Option({
-    flags: '-d --data-source <string>',
-    description: 'The data you want transformed to RDF.',
-  })
-  parseDataSource(val: string) {
-    return val;
   }
 
   @Option({
     flags: '-m --mapping <string>',
     description: 'The name of the mapping file.',
   })
-  parseRml(val: string) {
-    return val;
+  parseMapping(val: string) {
+    return path.join(__dirname, '../../../../resources/mappings/', val);
   }
 
   @Option({
@@ -35,39 +27,31 @@ export class TransformCommand implements CommandRunner {
     description: 'What the output file should be called.',
   })
   parseOutput(val: string) {
-    return val;
+    return path.join(__dirname, '../../../../resources/rdf/', val);
   }
 
   transform = async (cliOptions) => {
-    const mapping = path.join(
-      __dirname,
-      '../../../../resources/mappings/',
-      cliOptions.mapping,
-    );
-
-    const output = path.join(
-      __dirname,
-      '../../../../resources/rdf/',
-      cliOptions.output,
-    );
-
     const options = {
+      /*
+      If you want n-quads instead of json as output,
+      you need to define toRDF to true in the options parameter
+      */
       toRDF: false,
       functions: {
-        createDescription: function (data) {
-          const description =
-            data[0] + ' ' + data[1] + ' is ' + data[2] + ' years old.';
-          return description;
-        },
-        createFullName: function (data) {
-          const fullName = data[1] + ', ' + data[0];
-          return fullName;
+        combineFields: function (data: string[]) {
+          let combined = '';
+
+          data.forEach((field: string) => {
+            combined += ' ' + field;
+          });
+
+          return combined.trimStart();
         },
       },
     };
 
     const result = await parser
-      .parseFile(mapping, output, options)
+      .parseFile(cliOptions.mapping, cliOptions.output, options)
       .catch((err) => {
         console.log(err);
       });
